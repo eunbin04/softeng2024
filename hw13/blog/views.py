b2dd48c8549a9a274ccca0195206dee22d2d6881
eunbin04 <153293377+eunbin04@users.noreply.github.com>
2024-12-02 12:50:22 +0000

    13주차 과제 코드

--- hw13/blog/views.py
+++ hw13/blog/views.py
@@ -1,24 +1,38 @@
 from django.shortcuts import redirect, render
 from django.views.generic import ListView, DetailView
+from django.core.paginator import Paginator
 from .models import Post, Category, Tag
 from .forms import CommentForm
 
 # Create your views here.
 def category_posts(request, category_id):
     category =Category.objects.get(id=category_id)
-    posts = Post.objects.filter(category=category)
-    return render(request, 'blog/category_posts.html', {'category': category, 'posts':posts, 'categories': Category.objects.all()})
+    posts = Post.objects.filter(category=category).order_by('-created_at')
+    paginator = Paginator(posts, 4)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'blog/category_posts.html',
+    {
+        'category': category,
+        'page_obj': page_obj,
+        'posts':posts,
+        'categories': Category.objects.all(),
+    })
 
 def tag_page(request, slug):
     tag = Tag.objects.get(slug=slug)
-    post_list = tag.posts.all()
+    post_list = tag.posts.all().order_by('-created_at')
     post_count = tag.posts.count()
+    paginator = Paginator(post_list, 4)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
     return render(
         request,
         'blog/tag_page.html',
         {
             'tag': tag,
+            'page_obj': page_obj,
             'post_list': post_list,
             'post_count': post_count,
             'categories': Category.objects.all(),
@@ -29,7 +43,7 @@ def tag_page(request, slug):
 class PostList(ListView):
     model = Post
     ordering = '-pk'
-    paginate_by = 5
+    paginate_by = 4
     context_object_name = 'posts'
 
     def get_context_data(self, **kwargs):
@@ -57,4 +71,4 @@ class PostDetail(DetailView):
             comment.save()
             return redirect(self.object.get_absolute_url())
         return self.get(request, *args, **kwargs)
-
+    
