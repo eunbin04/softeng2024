b5888c4691ac871bb87d3171d05ac0dd7058183d
eunbin04 <153293377+eunbin04@users.noreply.github.com>
2024-12-20 05:27:06 +0000

    합치기전

--- fin_softeng2024/do_it_django_prj/settings.py
+++ fin_softeng2024/do_it_django_prj/settings.py
@@ -118,6 +118,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL ='/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR, '_media')
